9cb6b4d217d6b5f62ede2b23e08cf0119cc66dda
David Gómez Chiu <david.gomez.chiu@gmail.com>
2020-05-22 20:05:47 -0500

    tarea hecha

---
+++ cypress/integration/my-tests/gd-9.spec.js
@@ -0,0 +1,19 @@
+describe('https://www.nafin.com/portalnf/content/herramientas-de-negocio/simulador-de-creditos/simulador-de-creditos.do', () => {
+    describe('Submit form', () => {
+        it('Should count the result rows after submit', () => {
+            // entrar al sitio
+            cy.visit('https://www.nafin.com/portalnf/content/herramientas-de-negocio/simulador-de-creditos/simulador-de-creditos.do');
+            // llenar la forma
+            cy.get('#dispDate').clear().type('05/05/2020');
+            cy.get('#creditAmount').clear({force:true}).type('20000');
+            cy.get('#paymentMethod').select('Mensual');
+            cy.get('#period').select('2');
+            cy.get('#rate').clear().type('15');
+            cy.contains('Calcular').click();
+            // modal
+            cy.contains('Ahora no').click();
+            cy.get('#resultadosSimulador>tbody').children().should('have.length',24);
+            // contar filas
+        });
+    });
+}); 
